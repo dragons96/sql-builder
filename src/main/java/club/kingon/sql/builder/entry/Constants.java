d0b74829ab1cec44a517a84d968b270a1f193a77
dragons96 <521274311@qq.com>
2022-03-14 13:04:09 +0800

    update

--- src/main/java/club/kingon/sql/builder/entry/Constants.java
+++ src/main/java/club/kingon/sql/builder/entry/Constants.java
@@ -1,7 +1,5 @@
 package club.kingon.sql.builder.entry;
 
-import java.util.function.Supplier;
-
 /**
  * @author dragons
  * @date 2021/11/10 17:00
@@ -9,10 +7,6 @@ import java.util.function.Supplier;
 public interface Constants {
 
     String EMPTY_STRING = "";
-
-    Supplier<Boolean> TRUE_PREDICATE = () -> true;
-
-    Supplier<Boolean> FALSE_PREDICATE = () -> false;
 
     Object[] EMPTY_OBJECT_ARRAY = new Object[0];
 
