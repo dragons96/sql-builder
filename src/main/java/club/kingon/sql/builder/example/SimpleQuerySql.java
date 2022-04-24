871081c97d4e4cebe11d719e64cd8c37e24e98c1
dragons96 <521274311@qq.com>
2022-04-24 12:26:40 +0800

    update

--- src/main/java/club/kingon/sql/builder/example/SimpleQuerySql.java
+++ src/main/java/club/kingon/sql/builder/example/SimpleQuerySql.java
@@ -23,7 +23,7 @@ public class SimpleQuerySql {
             .or(Conditions.where("t1.c", Operator.IN, 3, 4, 5).and("t2.c", Operator.NOT_BETWEEN_AND, 5, 10))
             .andLe("t1.d", SqlBuilder.select("count(*)").from("t3").where("t3.a < ?", 100))
             .andLike("t1.b", 1)
-            .and(true, Conditions.whereLe("id", 10))
+            .and(true, Conditions.whereLe("id", 10), Conditions.whereLe("ip", 20))
             .groupBy("t1.z")
             .having("count(1)", Operator.GE, 100)
             .orderBy("t1.z", Order.ASC)
